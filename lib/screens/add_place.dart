06582745de4dafa480e031db0ee2df2b1012b25c
Kotlintpoint <ankit.sodha123@gmail.com>
2025-04-09 13:22:47 +0530

    - Adding "location" package and Starting with the "Get Location" Input Widget

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_favourite_places/providers/user_places.dart';
 import 'package:flutter_favourite_places/widgets/image_input.dart';
+import 'package:flutter_favourite_places/widgets/location_input.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class AddPlaceScreen extends ConsumerStatefulWidget {
@@ -57,6 +58,8 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
                 _selectedImage = image;
               },
             ),
+            const SizedBox(height: 16),
+            LocationInput(),
             const SizedBox(height: 16),
             ElevatedButton.icon(
               onPressed: _savePlace,
