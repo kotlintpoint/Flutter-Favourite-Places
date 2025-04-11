42b368b0c99c8a2b4e601f3ba88ed27f9a6234b6
Kotlintpoint <ankit.sodha123@gmail.com>
2025-04-11 20:26:24 +0530

    - Using Picked Location in Form

--- lib/models/Place.dart
+++ lib/models/Place.dart
@@ -20,7 +20,8 @@ class Place {
   final String id;
   final String title;
   final File image;
+  final PlaceLocation location;
 
-  Place({required this.title, required this.image})
+  Place({required this.title, required this.image, required this.location})
     : id = uuid.v4();
 }
--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_favourite_places/models/Place.dart';
 import 'package:flutter_favourite_places/providers/user_places.dart';
 import 'package:flutter_favourite_places/widgets/image_input.dart';
 import 'package:flutter_favourite_places/widgets/location_input.dart';
@@ -18,6 +19,7 @@ class AddPlaceScreen extends ConsumerStatefulWidget {
 class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
   final _titleController = TextEditingController();
   File? _selectedImage;
+  PlaceLocation? _selectedLocation;
 
   @override
   void dispose() {
@@ -27,13 +29,15 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
 
   void _savePlace() {
     final enteredTitle = _titleController.text;
-    if (enteredTitle.isEmpty || _selectedImage == null) {
+    if (enteredTitle.isEmpty ||
+        _selectedImage == null ||
+        _selectedLocation == null) {
       return;
     }
 
     ref
         .read(userPlacesProvider.notifier)
-        .addPlace(enteredTitle, _selectedImage!);
+        .addPlace(enteredTitle, _selectedImage!, _selectedLocation!);
 
     Navigator.of(context).pop();
   }
@@ -59,7 +63,11 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
               },
             ),
             const SizedBox(height: 16),
-            LocationInput(),
+            LocationInput(
+              onSelectLocation: (location) {
+                _selectedLocation = location;
+              },
+            ),
             const SizedBox(height: 16),
             ElevatedButton.icon(
               onPressed: _savePlace,
