ca68d061877dceb79508f96c493627dcfd947c68
Kotlintpoint <ankit.sodha123@gmail.com>
2025-04-11 19:53:05 +0530

    - Displaying location preview Map snapshot via google

--- lib/models/Place.dart
+++ lib/models/Place.dart
@@ -9,17 +9,18 @@ class PlaceLocation {
   final double longitude;
   final String address;
 
-  PlaceLocation({required this.latitude, required this.longitude, required this.address});
-
+  PlaceLocation({
+    required this.latitude,
+    required this.longitude,
+    required this.address,
+  });
 }
 
-class Place
-{
+class Place {
   final String id;
   final String title;
   final File image;
-  final PlaceLocation location;
 
-
-  Place({required this.title, required this.image, required this.location}) : id = uuid.v4();
+  Place({required this.title, required this.image})
+    : id = uuid.v4();
 }
--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -16,6 +16,20 @@ class _LocationInputState extends State<LocationInput> {
   PlaceLocation? _pickedLocation;
   var _isGettingLocation = false;
 
+  String get locationImage {
+    // Google Maps Static API
+    // https://developers.google.com/maps/documentation/maps-static/overview
+
+    if (_pickedLocation == null) {
+      return '';
+    }
+
+    final lat = _pickedLocation!.latitude;
+    final lng = _pickedLocation!.longitude;
+
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=YOUR_API_KEY';
+  }
+
   void _getCurrentLocation() async {
     Location location = Location();
 
@@ -77,6 +91,15 @@ class _LocationInputState extends State<LocationInput> {
         color: Theme.of(context).colorScheme.onSurface,
       ),
     );
+
+    if (_pickedLocation != null) {
+      previewContent = Image.network(
+        locationImage,
+        fit: BoxFit.cover,
+        width: double.infinity,
+        height: double.infinity,
+      );
+    }
 
     if (_isGettingLocation) {
       previewContent = const CircularProgressIndicator();
